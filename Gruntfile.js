22ef837a5a203e083c62c1eaed1f46016b37fe59
mai.shu <ansiboy@163.com>
2019-04-16 21:02:14 +0800

    require 打包

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,11 +28,51 @@ module.exports = function (grunt) {
                 browserifyOptions: {
                     standalone: pkg.name,
                 },
-                external: ['react', 'react-dom'],
+                external: ['react', 'react-dom', 'maishu-chitu', 'maishu-chitu-service'],
                 transform: ['browserify-css'],
-                alias: [
-                    `./node_modules/maishu-chitu/out/index.js:maishu-chitu`,
-                ],
+                // alias: [
+                //     `./node_modules/maishu-chitu/out/index.js:maishu-chitu`,
+                //     `./node_modules/maishu-chitu-service/out/index.js:maishu-chitu-service`,
+                // ],
+            }
+        },
+        concat: {
+            chitudts: {
+                options: {
+                    stripBanners: true,
+                    banner: license
+                },
+                src: ['./dist/index.js'],
+                dest: './dist/index.js'
+            },
+
+        },
+        requirejs: {
+            dev: {
+                options: {
+                    baseUrl: `./`,
+                    include: ['./out/index.js'],
+                    exclude: [
+                        "maishu-chitu",
+                        "maishu-chitu-service",
+                        "react",
+                        "react-dom"
+                    ],
+                    // name: 'maishu-chitu-react',
+                    out: `dist/index.js`,
+                    optimize: "none",
+                    paths: {
+                        "maishu-chitu-service": "empty:",
+                        "maishu-chitu": "empty:",
+                        "react": "empty:",
+                        "react-dom": "empty:"
+                    },
+                    shim: {
+
+
+                    },
+
+                },
             }
         },
         shell: {
@@ -42,5 +82,5 @@ module.exports = function (grunt) {
         },
     })
 
-    grunt.registerTask('default', ['shell', 'browserify']);
+    grunt.registerTask('default', ['shell', 'requirejs', 'concat']);
 }
