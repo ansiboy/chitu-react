fb8acbe5babc8d85b2617d628226c9cb38a6d51f
maishu <ansiboy@163.com>
2019-05-07 20:49:39 +0800

    使用 amd

--- out/application.js
+++ out/application.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -7,52 +6,51 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOM = require("react-dom");
-const chitu = require("maishu-chitu");
-const errors_1 = require("./errors");
-class Page extends chitu.Page {
-    constructor() {
-        super(...arguments);
-        this.component = null;
+define(["require", "exports", "react", "react-dom", "maishu-chitu", "./errors"], function (require, exports, React, ReactDOM, chitu, errors_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    class Page extends chitu.Page {
+        constructor() {
+            super(...arguments);
+            this.component = null;
+        }
     }
-}
-exports.Page = Page;
-class Application extends chitu.Application {
-    constructor(args) {
-        super(args);
-        this.pageCreated.add((sender, page) => {
-            page.element.className = "page";
-        });
-    }
-    createDefaultAction(url, loadjs) {
-        return (page) => __awaiter(this, void 0, void 0, function* () {
-            let actionExports = yield loadjs(url);
-            if (!actionExports)
-                throw errors_1.Errors.exportsCanntNull(url);
-            let _action = actionExports['default'];
-            if (_action == null) {
-                throw errors_1.Errors.canntFindAction(page.name);
-            }
-            let action;
-            if (!chitu.PageMaster.isClass(_action)) {
-                return _action(page, this);
-            }
-            action = _action;
-            let app = this;
-            let props = {
-                app,
-                data: page.data,
-                source: page,
-                createService(type) {
-                    return page.createService(type);
+    exports.Page = Page;
+    class Application extends chitu.Application {
+        constructor(args) {
+            super(args);
+            this.pageCreated.add((sender, page) => {
+                page.element.className = "page";
+            });
+        }
+        createDefaultAction(url, loadjs) {
+            return (page) => __awaiter(this, void 0, void 0, function* () {
+                let actionExports = yield loadjs(url);
+                if (!actionExports)
+                    throw errors_1.Errors.exportsCanntNull(url);
+                let _action = actionExports['default'];
+                if (_action == null) {
+                    throw errors_1.Errors.canntFindAction(page.name);
                 }
-            };
-            let element = React.createElement(action, props);
-            let component = ReactDOM.render(element, page.element);
-            page.component = component;
-        });
+                let action;
+                if (!chitu.PageMaster.isClass(_action)) {
+                    return _action(page, this);
+                }
+                action = _action;
+                let app = this;
+                let props = {
+                    app,
+                    data: page.data,
+                    source: page,
+                    createService(type) {
+                        return page.createService(type);
+                    }
+                };
+                let element = React.createElement(action, props);
+                let component = ReactDOM.render(element, page.element);
+                page.component = component;
+            });
+        }
     }
-}
-exports.Application = Application;
+    exports.Application = Application;
+});
